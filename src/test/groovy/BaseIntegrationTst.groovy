d4385d1e7555fe70337872115ee7bfa6f3a3e401
Iain Porter <changeant@gmail.com>
2014-03-20 09:39:54 +0000

    Add IT tests and emma

--- src/test/groovy/BaseIntegrationTst.groovy
+++ src/test/groovy/BaseIntegrationTst.groovy
@@ -4,7 +4,7 @@ import groovyx.net.http.RESTClient
 
 class BaseIntegrationTst extends GroovyTestCase {
 
-    private def BASE_URL = "http://localhost:8080/"
+    private def BASE_URL = "http://localhost:8080/oauth2-provider/"
     protected def USER_PATH = "v1.0/user"
     protected def ME_PATH = "v1.0/me"
     private def OAUTH_TOKEN_PATH = "oauth/token"
