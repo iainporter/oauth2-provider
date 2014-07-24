3b050f6cf0f9cf512fe9757855b3e9cef58aac52
Iain Porter <changeant@gmail.com>
2014-07-24 10:44:35 +0100

    Register CORS filter Fix health check

--- src/main/java/com/porterhead/filter/CrossOriginResourceSharingFilter.java
+++ src/main/java/com/porterhead/filter/CrossOriginResourceSharingFilter.java
@@ -4,19 +4,24 @@
  */
 package com.porterhead.filter;
 
+
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
+import org.springframework.stereotype.Component;
+
 import javax.ws.rs.ext.Provider;
 
 @Provider
+@Component
 public class CrossOriginResourceSharingFilter implements ContainerResponseFilter {
 
     @Override
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
         response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
         response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "X-HTTP-Method-Override,Content-Type");
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "X-HTTP-Method-Override, Content-Type, x-requested-with");
+        response.getHttpHeaders().putSingle("Access-Control-Max-Age", "3600");
         return response;
     }
 
