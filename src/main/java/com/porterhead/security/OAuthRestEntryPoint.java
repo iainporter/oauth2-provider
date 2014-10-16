f141801ffb9c85f26e8cc4f440357f45b9b4525a
Iain Porter <changeant@gmail.com>
2014-10-16 15:57:33 +0100

    Add Custom Entry Point to provide REST response for Basic Auth failures

---
+++ src/main/java/com/porterhead/security/OAuthRestEntryPoint.java
@@ -0,0 +1,22 @@
+package com.porterhead.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by iainporter on 16/10/2014.
+ */
+@Component
+public class OAuthRestEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Bad Credentials");
+    }
+}
